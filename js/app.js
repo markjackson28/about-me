aa81fec0a858c34c0281f2c933146263d87e349b
Mark Thanadabouth <mark28ten@gmail.com>
2021-05-08 19:47:20 -0700

    updated comments and css

--- js/app.js
+++ js/app.js
@@ -1,93 +1,75 @@
 'use strict';
 
-// console.log('Hello World')
-
 let totalScore = 0;
-
-// Welcome Prompt
 
 let username = prompt('What is your name?');
 alert('Welcome ' + username + '! My name is Mark. Nice to me meet you! Please answer the following five questions with yes/no or y/n. Please and Thank You!');
 
-// First Question
 function q1() {
   let answerOne = prompt('Do I live in Seattle?').toLowerCase();
 
   if (answerOne === 'yes' || answerOne === 'y') {
-  // console.log('You are correct! I live in Seattle');
     alert('You are correct! I do live in Seattle!');
     totalScore++;
   }
 }
 q1();
-// Second Question
+
 function q2() {
   let answerTwo = prompt('Was I ever a waiter?').toLowerCase();
 
   if (answerTwo === 'no' || answerTwo === 'n') {
-  // console.log('You are correct! I was not a waiter, I was a bartender.');
     alert('You are correct! I was not a waiter, I was a bartender.');
     totalScore++;
   }
 }
 q2();
-// Third Question
+
 function q3() {
   let answerThree = prompt('Is it one of my goals to compete in the CrossFit games?').toLowerCase();
 
   if (answerThree === 'yes' || answerThree === 'y') {
-  // console.log('You are correct! I hope to compete in the CrossFit Games.');
     alert('You are correct! I hope to compete in the CrossFit Games.');
     totalScore++;
   }
 }
 q3();
 
-// Fourth Question
 function q4 () {
   let answerFour = prompt('Did I live in Canada before Seattle?').toLowerCase();
 
   if (answerFour === 'no' || answerFour === 'n') {
-  // console.log('You are correct! I lived in Tacoma before Seattle.');
-  // console.log('answerFour: ' + answerFour)
     alert('You are correct! I lived in Tacoma before Seattle.');
     totalScore++;
   }
 }
 q4();
-// Fifth Question
+
 function q5 () {
   let answerFive = prompt('Did I ever serve in the military?').toLowerCase();
 
   if (answerFive === 'yes' || answerFive === 'y') {
-  // console.log('You are correct! I did serve in the military!');
     alert('You are correct! I did serve in the military!');
     totalScore++;
   }
 }
 q5();
 
-// Sixth Question
 function q6 () {
   alert('I have a picked a number between 1-15. You will have 4 chances to guess the correct number. Have fun!');
 
   for (let i = 4; i > 0; i--){
     let answerSix = parseInt(prompt('Please pick a number 1-15.'));
-    // console.log(I have a picked a number between 1-15. You will have 4 chances to guess the correct number. Have fun!');
     if (answerSix === 7){
-    // console.log('You are correct!');
       alert('You are correct! Great Job!');
       totalScore++;
       break;
     }
     else if (answerSix > 7) {
-    // console.log('Sorry, too high. Please try again.');
       alert('Sorry, too high. Please try again.');
     } else if (answerSix < 7) {
-    // console.log('Sorry, too low. Please try again');
       alert('Sorry, too low. Please try again');
     } if (i === 1){
-      // console.log('Sorry, you did not guess the correct number. Good Try!');
       alert('Sorry, you did not guess the correct number. My was 7. Good try!');
       break;
     }
@@ -95,7 +77,6 @@ function q6 () {
 }
 q6();
 
-// Seventh Question
 function q7(){
   let answeredCorrectly = false;
   let attempts = 6;
@@ -107,20 +88,17 @@ function q7(){
 
     for (let i = 0; i < favColors.length; i++){
       if (answerSeven === favColors[i]) {
-      // console.log('You are correct!');
         alert('You are correct! Great Job!');
         answeredCorrectly = true;
         totalScore++;
       }
     }
     if (attempts === 1){
-    //console.log('Sorry you did not answer Correctly. My answers were red, white, and blue. Good try though!')
       alert('Sorry you did not answer Correctly. My answers were red, white, and blue. Good try though!');
     }
     attempts--;
   }
 
-  alert('You scored ' + totalScore + ' out of 7 questions. Thanks for playing ' + username + '!');
-// console.log('Score ' + totalScore)
+  alert('You scored ' + totalScore + ' out of 7 questions. Thanks for playing ' + username + '!')
 }
 q7();
